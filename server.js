164ccca56dcc28f75f1e5d90297d45c6c815634d
chirazboudh <37347461+chirazboudh@users.noreply.github.com>
2019-03-11 10:08:31 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Fast html framework
+var express = require('express');
+var app = express();
+
+// For receiving JSON in posts
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// For the database
+var sqlite3 = require('sqlite3'); //.verbose();
+var db = new sqlite3.Database('./db/projet.db');
+
+// Add restful controller
+require('./wifiController')(app, db, jsonParser);
+
+// Serve static files
+app.use(express.static('wwwroot'))
+
+ var server = app.listen(process.env.PORT || 3000, function () {
+
+ var port = server.address().port;
+    console.log("App now running on port", port);
+
+});
